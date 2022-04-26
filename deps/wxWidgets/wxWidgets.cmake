4c2a43296c95711034bb810805501fcf91ab85a3
YuSanka <yusanka@gmail.com>
2022-04-26 15:53:28 +0200

    Fixes to support wxWidgets 3.1.6

--- deps/wxWidgets/wxWidgets.cmake
+++ deps/wxWidgets/wxWidgets.cmake
@@ -1,4 +1,4 @@
-set(_wx_git_tag v3.1.4-patched)
+set(_wx_git_tag v3.1.6-patched)
 
 set(_wx_toolkit "")
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
@@ -12,8 +12,8 @@ endif()
 prusaslicer_add_cmake_project(wxWidgets
     # GIT_REPOSITORY "https://github.com/prusa3d/wxWidgets"
     # GIT_TAG tm_cross_compile #${_wx_git_tag}
-    URL https://github.com/prusa3d/wxWidgets/archive/489f6118256853cf5b299d595868641938566cdb.zip
-    URL_HASH SHA256=5b22d465377cedd8044bba69bea958b248953fd3628c1de4913a84d4e6f6175b
+    URL https://github.com/prusa3d/wxWidgets/archive/693b9dc2e1a73054cfff5fa40f08c26504ef3e32.zip
+    URL_HASH SHA256=68d94c99729799a549f074f763507dd91b5e632d110f7bcbbbadd95dbdf1a64c
     DEPENDS ${PNG_PKG} ${ZLIB_PKG} ${EXPAT_PKG} dep_TIFF dep_JPEG
     CMAKE_ARGS
         -DwxBUILD_PRECOMP=ON
--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -2074,6 +2074,7 @@ bool GUI_App::load_language(wxString language, bool initial)
         {
 	    	// Allocating a temporary locale will switch the default wxTranslations to its internal wxTranslations instance.
 	    	wxLocale temp_locale;
+            temp_locale.Init();
 	    	// Set the current translation's language to default, otherwise GetBestTranslation() may not work (see the wxWidgets source code).
 	    	wxTranslations::Get()->SetLanguage(wxLANGUAGE_DEFAULT);
 	    	// Let the wxFileTranslationsLoader enumerate all translation dictionaries for PrusaSlicer
@@ -2214,7 +2215,7 @@ void GUI_App::update_mode()
 {
     sidebar().update_mode();
 
-#ifdef _MSW_DARK_MODE
+#ifdef _WIN32 //_MSW_DARK_MODE
     if (!wxGetApp().tabs_as_menu())
         dynamic_cast<Notebook*>(mainframe->m_tabpanel)->UpdateMode();
 #endif
