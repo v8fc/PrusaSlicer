b98ca97dcf401c26ea9f28398c0d11840eb08ace
tamasmeszaros <meszaros.q@gmail.com>
2022-05-05 14:45:12 +0200

    Fix the nanosvg linking in-house by putting nsvg functions into Slic3r::

--- src/slic3r/GUI/BitmapCache.hpp
+++ src/slic3r/GUI/BitmapCache.hpp
@@ -9,7 +9,7 @@
     #include <wx/wx.h>
 #endif
 
-struct NSVGimage;
+#include "nanosvg/nanosvg.h"
 
 namespace Slic3r { namespace GUI {
 
@@ -38,7 +38,7 @@ public:
 	// Parses SVG file from a file, returns SVG image as paths.
 	// And makes replases befor parsing
 	// replace_map containes old_value->new_value
-	static NSVGimage* nsvgParseFromFileWithReplace(const char* filename, const char* units, float dpi, const std::map<std::string, std::string>& replaces);
+    static NSVGimage* nsvgParseFromFileWithReplace(const char* filename, const char* units, float dpi, const std::map<std::string, std::string>& replaces);
 	// Load svg from resources/icons. bitmap_key is given without the .svg suffix. SVG will be rasterized to provided height/width.
     wxBitmap* 		load_svg(const std::string &bitmap_key, unsigned width = 0, unsigned height = 0, const bool grayscale = false, const bool dark_mode = false, const std::string& new_color = "");
 
